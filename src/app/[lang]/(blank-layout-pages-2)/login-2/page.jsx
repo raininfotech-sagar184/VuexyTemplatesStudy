ad748eeea3172eca5545027ca905937570893068
raininfotech-sagar184 <sagar184@webmail.rainit.in>
2024-05-11 17:21:35 +0530

    -

---
+++ src/app/[lang]/(blank-layout-pages-2)/login-2/page.jsx
@@ -0,0 +1,20 @@
+// Component Imports
+
+
+// Server Action Imports
+import { getServerMode } from '@core/utils/serverHelpers'
+import Login from '../../../../views/Login'
+
+export const metadata = {
+  title: 'Login',
+  description: 'Login to your account'
+}
+
+const LoginPage = () => {
+  // Vars
+  const mode = getServerMode()
+
+  return <Login mode={mode} />
+}
+
+export default LoginPage
